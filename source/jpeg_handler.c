af04446441d6d3b220eb81d66d72e31b7731ed2a
eburhan <130926828+eburhansjah@users.noreply.github.com>
2024-10-24 19:14:55 -0400

    Create jpeg_handler.c that loads jpeg files

---
+++ source/jpeg_handler.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "jpeg_handler.h"
+#include "stb_image.h"
+
+// Fn. that loads jpeg files with stb_image
+Image* load_jpeg(const char *filename){
+    Image *img = (Image *)malloc (sizeof(Image));
+    img -> data = stbi_load(filename, &img->width, &img->height, &img->channels, 0);
+
+    if(img->data == NULL){
+        printf("Failed to load image: %s\n", filename);
+        free(img);
+
+        return NULL;
+    }
+    printf("Loaded JPEG: %s (%dx%d, %d channels)\n", filename, img->width, img->height, img->channels);
+    
+    return img;
+}
+
+// Fn. that frees allocated mem. for img
+void free_image(Image *img){
+    if(img){
+        stbi_image_free(img->data);
+        free(img);
+    }
+}
