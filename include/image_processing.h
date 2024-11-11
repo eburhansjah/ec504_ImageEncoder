4f4b372f2323226dbe483fd79e8acf94487f71e9
eburhan <130926828+eburhansjah@users.noreply.github.com>
2024-11-11 17:13:39 -0500

    Added: IDCT (in progress) & FFT based DCT. Modified extract_8x8_block, de/quantization, and zigzag_scanning to work with [8][8] block instead of [64]

--- include/image_processing.h
+++ include/image_processing.h
@@ -8,9 +8,14 @@ int check_dimensions(Image *images[], int count);
 void convert_rgb_to_ycbcr(Image *img, unsigned char **Y, unsigned char **Cb, unsigned char **Cr); // RGB to YCbCr color space
 void write_to_bitstream(const char *filename, unsigned char *Y, unsigned char *Cb, unsigned char *Cr, int width, int height);
 void subsampling_420(unsigned char *Cb, unsigned char *Cr, int width, int height, unsigned char **Cb_sub, unsigned char **Cr_sub);
-void extract_8x8_block(unsigned char *channel, int image_width, int start_x, int start_y, unsigned char block[64]);
+void extract_8x8_block(unsigned char *channel, int image_width, int start_x, int start_y, unsigned char block[8][8]);
+
 void DCT(const unsigned char block[64], float dct_block[64]);
-void quantization(float dct_block[64], int quantized_block[64]);
-void zigzag_scanning(int quantized_block[64], int zigzag_block[64]);
+void fast_DCT(const unsigned char block[8][8], double dct_block[8][8]);
+void quantization(double dct_block[8][8], int quantized_block[8][8]);
+void zigzag_scanning(int quantized_block[8][8], int zigzag_array[64]);
+
+void dequantization(int quantized_block[8][8], double dct_block[8][8]);
+void IDCT(const float dct_block[64], unsigned char block[64]);
 
 #endif // IMAGE_PROCESSING_H
